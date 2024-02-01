92b3f863432c276b960613b992500a99cb9307d3
Michael Lo <michael.lo@buyandship.com.tw>
2024-02-02 05:33:29 +0800

    Add gulpfile.js for path replacement

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+import gulp from 'gulp'
+import gulpReplace from 'gulp-replace'
+
+gulp.task('replacePath', () => {
+  return gulp.src('/vercel/path0/.vercel/output/static/index.html')
+    .pipe(gulpReplace(/\/_nuxt\/([a-zA-Z0-9_.-]+)\b/g, 'https://final-nuxt3-app.vercel.app/_nuxt/$1'))
+    .pipe(gulp.dest('/vercel/path0/.vercel/output/static'))
+
+  // return gulp.src('.output/public/index.html')
+  //   .pipe(gulpReplace(/\/_nuxt\/([a-zA-Z0-9_.-]+)\b/g, 'https://pcs-nuxt3-app-ahzv.vercel.app/_nuxt/$1'))
+  //   .pipe(gulp.dest('.output/public'))
+})
