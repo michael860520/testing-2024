c15e689e7034f97d11401ee98ef7fb15e48f87e2
Michael Lo <michael.lo@buyandship.com.tw>
2024-02-02 05:07:09 +0800

    Add color mode selection and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxtjs/i18n"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    '@nuxtjs/color-mode',
+    "@pinia/nuxt",
+    "@nuxtjs/i18n"],
+  colorMode: {
+    classSuffix: ''
+  },
   i18n: {
     langDir: 'locales',
     locales: [
