9a39c3a87054646c863372dfed847b207e61d742
Michael Lo <michael.lo@buyandship.com.tw>
2024-02-14 10:04:24 +0800

    Update image URLs in Logos.vue

--- components/Logos.vue
+++ components/Logos.vue
@@ -1,14 +1,20 @@
+<script setup lang="ts">
+import {isProductionURL} from '@/utils'
+
+</script>
+
 <template>
   <div class="inline-flex cursor-default text-2xl font-light">
     <div class="flex flex-col">
-      <img class="inline-block h-14 w-14" src="https://pcs-nuxt3-v2.vercel.app/nuxt.svg">
+      <img class="inline-block h-14 w-14" :src="`${isProductionURL()}nuxt.svg`">
       <span class="mt-2 text-green-500">Nuxt 3</span>
     </div>
     <div
       class="text-3xl bg-grayz-500xw mx-4 my-auto transform transition-all-500 hover:rotate-135"
     />
     <div class="flex flex-col">
-      <img class="inline-block h-14 w-14" src="https://pcs-nuxt3-v2.vercel.app/vite.png">
+      <img class="inline-block h-14 w-14" :src="`${isProductionURL()}vite.png`">
+
       <span class="mt-2 text-purple-500">Vitesse</span>
     </div>
   </div>
