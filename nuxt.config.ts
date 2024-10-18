6da880beb361d16378ca1c30f85e205856298e33
michael860520 <michael.lo@buyandship.com.tw>
2024-10-19 03:34:50 +0800

    feat: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,6 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxtjs/i18n',
     '@nuxt/test-utils/module',
-    '@sentry/nuxt/module',
   ],
   colorMode: {
     classSuffix: '',
