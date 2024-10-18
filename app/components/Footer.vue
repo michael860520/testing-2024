59fecf9db5b8e5b09368e3e8d72252a52e66aba6
michael860520 <michael.lo@buyandship.com.tw>
2024-10-19 04:52:50 +0800

    feat: udpate

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
     <NuxtLink to="/" />
-    <a href="https://github.com/antfu/vitesse-nuxt3" target="_blank" />
+    <a href="https://github.com/antfu/vitesse-nuxt3" target="_blank" rel="noopener noreferrer" />
   </div>
 </template>
